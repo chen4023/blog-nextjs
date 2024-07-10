8d032cbe2918ce11820027b95379196cb656478e
chen4023 <106866926+chen4023@users.noreply.github.com>
2024-07-10 13:22:14 +0900

    [fix] conflict error

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,20 +8,6 @@ export default function Profile() {
       <Image
         src={profileImg}
         alt="profile"
-<<<<<<< HEAD
-        width={200}
-        height={200}
-        className="rounded-full"
-        priority // 해당 이미지는 중요하니까 이거 먼저 다운로드 받아줘
-      />
-      <h1 className="text-2xl font-bold">{"Hi, I'm Chen"}</h1>
-      <p className="text-lg font-semibold">Front-Engineer</p>
-      <p>꿈을 코딩하는 사람, 드림코더 엘리</p>
-      <Link href="/contact">
-        <button className=" bg-yellow-400 px-4 py-1 rounded-xl font-bold mt-3">
-          Contact Me
-        </button>
-=======
         width={250}
         height={250}
         className="rounded-full mx-auto mb-2"
@@ -32,10 +18,8 @@ export default function Profile() {
       <p>꿈을 코딩하는 사람, 드림코더 엘리</p>
       <Link
         href="/contact"
-
       >
         <button className=" bg-yellow-400 px-3 py-1 rounded-xl font-bold mt-2">Contact Me</button>
->>>>>>> 10d767b83072d4e577ad70587f1b6e8f282e3a99
       </Link>
     </section>
   );
