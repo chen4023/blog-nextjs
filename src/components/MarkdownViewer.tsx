8dd44ee6326af6c73f9ad7edf889ce6a93be9028
chen4023 <ads4023@naver.com>
2024-07-15 18:56:22 +0900

    [fix] syntax-highlighter error

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,10 +1,35 @@
 "use client";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <Markdown className="prose lg:prose-xl mt-8" remarkPlugins={[remarkGfm]}>
+    <Markdown
+      className="prose lg:prose-xl mt-8"
+      remarkPlugins={[remarkGfm]}
+      components={{
+        code(props) {
+          const { children, className, node, ref, ...rest } = props;
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              {...rest}
+              PreTag="div"
+              language={match[1]}
+              style={dark}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...rest} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {content}
     </Markdown>
   );
