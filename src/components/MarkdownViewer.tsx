9faa044572a88e6e3d996b73290787f375638855
chen4023 <ads4023@naver.com>
2024-07-15 19:00:16 +0900

    [style] syntax highlighter style

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,7 +2,7 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
@@ -18,7 +18,7 @@ export default function MarkdownViewer({ content }: { content: string }) {
               {...rest}
               PreTag="div"
               language={match[1]}
-              style={dark}
+              style={materialDark}
             >
               {String(children).replace(/\n$/, "")}
             </SyntaxHighlighter>
