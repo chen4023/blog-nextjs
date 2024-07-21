f9efa5ebcdfb9d0d163c4aad723d28bfc08296d3
현채은 <ads4023@naver.com>
2024-07-21 16:56:01 +0900

    [design] profile 텍스트 수정

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -13,9 +13,9 @@ export default function Profile() {
         className="rounded-full mx-auto mb-2"
         priority // 해당 이미지는 중요하니까 우선순위로 다운로드 해줘
       />
-      <h1 className="text-3xl font-bold">{"Hi, I'm Chen"}</h1>
+      <h1 className="text-3xl font-bold">{`"안녕하세요, 저는 프론트엔드 개발자 현채은입니다."`}</h1>
       <p className="text-xl font-semibold">Front Engineer</p>
-      <p>꿈을 코딩하는 사람, 드림코더 엘리</p>
+      <p>꿈을 코딩하는 사람</p>
       <Link href="/contact">
         <button className=" bg-yellow-400 px-3 py-1 rounded-xl font-bold mt-2">
           Contact Me
